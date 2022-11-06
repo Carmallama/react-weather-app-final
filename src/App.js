8fe08cc20ee2e4a359f17965001de905bcf6f658
Carmallama <carmen.alger@hotmail.co.uk>
2022-11-06 11:21:06 +0000

    Added noreferrer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <h1>Weather app</h1>
       <footer>
         This project was coded by Carmen Alger and is{" "}
         <a
           href="https://github.com/Carmallama/weather-app-react"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced
         </a>
