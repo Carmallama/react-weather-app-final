cda5a93ad55773337cb5204482d50acc3b5ff69d
Carmallama <carmen.alger@hotmail.co.uk>
2022-11-06 11:34:41 +0000

    Added Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Carmen Alger and is{" "}
-        <a
-          href="https://github.com/Carmallama/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Carmen Alger and is{" "}
+          <a
+            href="https://github.com/Carmallama/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
