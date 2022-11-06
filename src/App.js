74bc4ea6c2c6a6bc73f9297ca9d816fcda1fed87
Carmallama <carmen.alger@hotmail.co.uk>
2022-11-06 11:17:04 +0000

    Added footer with link to Github

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by Carmen Alger and is{" "}
+        <a
+          href="https://github.com/Carmallama/weather-app-react"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
